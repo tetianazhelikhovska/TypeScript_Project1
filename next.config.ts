7c581074120e5ccbaa92aba3b71813a8379c3198
Tania Zhelikhovska <tanazelihovska406@gmail.com>
2026-05-27 15:42:44 +0300

    Add TypeScript project

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
-  basePath: "/TypeScript-labs",
-  assetPrefix: "/TypeScript-labs/",
-
   images: {
-    unoptimized: true,
-
     remotePatterns: [
       { protocol: 'https', hostname: 'images2.imgbox.com' },
       { protocol: 'https', hostname: '*.staticflickr.com' },
